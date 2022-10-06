f4f6bdedaa9e8913b733b444fdb064b0c387d8b8
panasiuk-r <rus67@ukr.net>
2022-10-06 11:17:12 +0300

    Homework2_PanasiukR.I._KI-03

--- threads.py
+++ threads.py
@@ -4,11 +4,12 @@ from time import time, sleep
 import random
 
 locker = threading.Lock()
-
-
+locker1 = threading.Lock()
+in=0
+ei=threading.Event()
 class MyClass1:
-    def __init__(self, f1=0, f2=0):
-        self.__f1 = f1
+    def init(self, f1=0, f2=0):
+        self.f1 = f1
         self.__f2 =f2
 
     def getF1(self):
@@ -22,8 +23,8 @@ class MyClass1:
         self.__f2=f2
 
 class MyClass2:
-    def __init__(self, f1=0, f2=0):
-        self.__f1 = f1
+    def __init(self, f1=0, f2=0):
+        self.f1 = f1
         self.__f2 =f2
 
     def getF1(self):
@@ -36,9 +37,9 @@ class MyClass2:
     def setF2(self, f2):
         self.__f2=f2
 
-def fu1(MC, K1, lock, event):
+def fu1(MC, K1, lock, event, in):
+    lock.acquire()
     try:
-        lock.acquire()
         print("thread has started")
         N1=0
         N2=0
@@ -49,10 +50,16 @@ def fu1(MC, K1, lock, event):
             nF2=N2+MC.getF2()
             MC.setF1(nF1)
             MC.setF2(nF2)
-        print("thread has finished")
+            print("thread has finished")
+        lock1.acquire()
+        try:
+            in=in-1
+            if in=0:
+                ei.set()
+        finally:
+            lock1.release()
+    finally:
         lock.release()
-        event.set()
-    except:
         print("An exception in locker occurred")
 
 
@@ -67,22 +74,19 @@ def main():
     e=0;
     MC1=MyClass1()
     MC2=MyClass2()
+    in=N
     for i in range(N):
-        e=threading.Event()
-        events.append(e)
         if i < N/2:
-            thr=Thread(target = fu1, args = (MC1,K1,locker,e,))
+            thr=Thread(target = fu1, args = (MC1,K1,locker,in,))
         elif i>= N/2:
-            thr=Thread(target = fu1, args = (MC2,K2,locker,e,))
+            thr=Thread(target = fu1, args = (MC2,K2,locker,in,))
         thrs.append(thr)
         thr.start()
     print("almost there")
-    for i in range(N):
-        if events[i].is_set()!=True:
-            events[i].wait()
+    ei.wait()
     print("Done")
     x2=time()
     print(x2-x1)
 
-if __name__=="__main__":
+if __name=="main":
     main()
